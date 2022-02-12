94e4316d4a9176b6fa9643c0cf78fcdb0801f27d
Noman5237 <anonyman637@gmail.com>
2022-02-13 01:08:43 +0600

    removed index.css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
