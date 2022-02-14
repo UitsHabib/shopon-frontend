3dcf381b88678c41bcdd5514dd3e9f681a79bb08
amanullhazoha <amanullhazoha3784@gmail.com>
2022-02-15 01:01:18 +0600

    Pull master branch.

--- src/index.js
+++ src/index.js
@@ -2,18 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 import App from './modules/core/app.component';
-import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import "font-awesome/css/font-awesome.css";
-
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "font-awesome/css/font-awesome.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "./style/style.css";
 
