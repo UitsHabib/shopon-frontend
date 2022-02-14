6f50d4bfbbeeae88ec22fc8a704d404cca5c8b39
Noman5237 <anonyman637@gmail.com>
2022-02-14 21:49:37 +0600

    minor styling update

--- src/modules/platform/services/components/serviceDetails.component.jsx
+++ src/modules/platform/services/components/serviceDetails.component.jsx
@@ -18,49 +18,51 @@ const ServiceDetails = () => {
 	} = useService({ id: serviceId });
 
 	return (
-		<div className="d-flex flex-column flex-wrap justify-content-sm-center">
-			<table className="table table-bordered table-striped w-50">
-				<thead>
-					<tr>
-						<th scope="row" colSpan="2" className="text-center">
-							{title}
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<th scope="row">Created By</th>
-						<td>{createdBy?.first_name}</td>
-					</tr>
-					<tr>
-						<th scope="row">Created At</th>
-						<td>
-							{createdAt
-								? formatRelative(Date.parse(createdAt), new Date())
-								: null}
-						</td>
-					</tr>
-					<tr>
-						<th scope="row">Updated By</th>
-						<td>{updatedBy?.first_name}</td>
-					</tr>
-					<tr>
-						<th scope="row">Updated At</th>
-						<td>
-							{createdAt
-								? formatRelative(Date.parse(updatedAt), new Date())
-								: null}
-						</td>
-					</tr>
-				</tbody>
-			</table>
-			<button
-				type="button"
-				onClick={() => history.push('/platform/services')}
-				className="btn btn-primary w-25"
-			>
-				Go Back
-			</button>
+		<div className="container">
+			<div className="d-flex flex-column justify-content-sm-center">
+				<table className="table table-bordered table-striped p-2">
+					<thead>
+						<tr>
+							<th scope="row" colSpan="2" className="text-center">
+								{title}
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th scope="row">Created By</th>
+							<td>{createdBy?.first_name}</td>
+						</tr>
+						<tr>
+							<th scope="row">Created At</th>
+							<td>
+								{createdAt
+									? formatRelative(Date.parse(createdAt), new Date())
+									: null}
+							</td>
+						</tr>
+						<tr>
+							<th scope="row">Updated By</th>
+							<td>{updatedBy?.first_name}</td>
+						</tr>
+						<tr>
+							<th scope="row">Updated At</th>
+							<td>
+								{createdAt
+									? formatRelative(Date.parse(updatedAt), new Date())
+									: null}
+							</td>
+						</tr>
+					</tbody>
+				</table>
+				<button
+					type="button"
+					onClick={() => history.push('/platform/services')}
+					className="btn btn-primary text-center m-auto p-2"
+				>
+					Go Back
+				</button>
+			</div>
 		</div>
 	);
 };
