937b486e689cbedb3a25fa05a929dd052f8d5f52
amanullhazoha <amanullhazoha3784@gmail.com>
2022-02-15 00:45:50 +0600

    jkasdjf

--- src/modules/core/PrivateRoute.js
+++ src/modules/core/PrivateRoute.js
@@ -1,27 +1,32 @@
-import React from "react";
-import { Route, Redirect } from "react-router-dom";
-import Navbar from "./components/navbar.component";
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import Navbar from './components/navbar.component';
 
-import getLoggedInUser from "./service/get-logged-in-user";
+import getLoggedInUser from './service/get-logged-in-user';
 
 export default function PrivateRoute({ component: Component, ...rest }) {
-    const loggedInUser = getLoggedInUser();
-    
-    return (
-        <Route {...rest} render={props => {
-            return (
-                loggedInUser ? (
-                    <>
-                        <Navbar />
-                        <Component {...props}/>
-                    </>
-                ) : (
-                    <Redirect push to={{
-                        pathname: "/login",
-                        state: { from: props.location }
-                    }} />
-                )
-            );
-        }}/>
-    )
+	const loggedInUser = getLoggedInUser();
+	console.log(rest);
+
+	return (
+		<Route
+			{...rest}
+			render={(props) => {
+				return loggedInUser ? (
+					<>
+						<Navbar path={rest.path} />
+						<Component {...props} />
+					</>
+				) : (
+					<Redirect
+						push
+						to={{
+							pathname: '/login',
+							state: { from: props.location },
+						}}
+					/>
+				);
+			}}
+		/>
+	);
 }
