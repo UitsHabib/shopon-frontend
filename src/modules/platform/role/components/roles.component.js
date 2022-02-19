b6049ae0f8a0081af4b30f5b93495d9e9d7305cf
NafiAsib <nafi.asib@gmail.com>
2022-02-19 21:07:03 +0600

    Refactor breadcrumb, role & idle logout.

--- src/modules/platform/role/components/roles.component.js
+++ src/modules/platform/role/components/roles.component.js
@@ -12,261 +12,259 @@ import Pagination from './common/pagination.component';
 Modal.setAppElement('#app');
 
 function Roles() {
-	const [roles, setRoles] = useState([]);
-	const [isLoaded, setIsLoaded] = useState(false);
-	const [sorters, setSorters] = useState({ key: 'id', order: 'asc' });
-	const [itemsPerPage, setItemsPerPage] = useState(5);
-	const [currentPage, setCurrentPage] = useState(1);
-	const [isModalDetails, setIsModalDetails] = useState(false);
-	const [currentRole, setCurrentRole] = useState({});
-	const [roleToDelete, setRoleToDelete] = useState({});
-	const [deleteConfirmation, setDeleteConfirmation] = useState(false);
+  const [roles, setRoles] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [sorters, setSorters] = useState({ key: 'id', order: 'asc' });
+  const [itemsPerPage, setItemsPerPage] = useState(5);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isModalDetails, setIsModalDetails] = useState(false);
+  const [currentRole, setCurrentRole] = useState({});
+  const [roleToDelete, setRoleToDelete] = useState({});
+  const [deleteConfirmation, setDeleteConfirmation] = useState(false);
 
-	const pageOptions = _.range(5, 25, 5);
+  const roleColumns = [
+    {
+      label: 'ID',
+      key: 'id',
+      sortable: 'true',
+      content: (role, key) => <th scope="row">{role[key]}</th>,
+    },
+    {
+      label: 'Title',
+      key: 'title',
+      sortable: 'true',
+      content: (role, key) => <td scope="row">{role[key]}</td>,
+    },
+    {
+      label: 'Created At',
+      key: 'created_at',
+      content: (role, key) => <td scope="row">{role[key]}</td>,
+    },
+    {
+      label: 'Action',
+      key: 'action',
+      content: (role, key) => (
+        <td scope="row">
+          <Dropdown>
+            <Dropdown.Toggle
+              variant=""
+              size="sm"
+              id="dropdown-basic"
+              bsPrefix="0"
+            >
+              {/* <i className="bi bi-pencil-square"></i> */}
+              <i className="bi bi-box-arrow-down text-success fa-lg" />
+            </Dropdown.Toggle>
 
-	const getRoles = async () => {
-		try {
-			const promise = axios.get('http://localhost:5000/api/roles', {
-				withCredentials: true,
-			});
-			const response = await promise;
-			setRoles(response.data);
-		} catch (error) {
-			console.log(error);
-		}
-		if (roles) setIsLoaded(true);
-	};
+            <Dropdown.Menu>
+              <Dropdown.Item onClick={() => handleDetails(role, key)}>
+                Details
+              </Dropdown.Item>
+              <Dropdown.Item as={Link} to={`/platform/roles/update/${role.id}`}>
+                Edit
+              </Dropdown.Item>
+              {/* <Dropdown.Item onClick={() => deleteRole(role.id)}> */}
+              <Dropdown.Item onClick={() => deleteWarning(role)}>
+                Delete
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </td>
+      ),
+    },
+  ];
 
-	const deleteRole = async (id) => {
-		try {
-			const response = await axios.delete(
-				`http://localhost:5000/api/roles/${id}`,
-				{ withCredentials: true }
-			);
-			const newRoles = [...roles].filter((role) => id !== role.id);
-			setRoles(newRoles);
-			setDeleteConfirmation(false);
-		} catch (error) {
-			console.error('Error: ', error);
-		}
-	};
+  const pageOptions = _.range(5, 25, 5);
 
-	const deleteWarning = (role) => {
-		setRoleToDelete(role);
-		setDeleteConfirmation((now) => !now);
-		console.log(deleteConfirmation);
-	};
+  const getRoles = async () => {
+    try {
+      const promise = axios.get('http://localhost:5000/api/roles', {
+        withCredentials: true,
+      });
+      const response = await promise;
+      setRoles(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+    if (roles) setIsLoaded(true);
+  };
 
-	const cancelDelete = () => {
-		setDeleteConfirmation((now) => !now);
-	};
+  const deleteRole = async (id) => {
+    try {
+      const response = await axios.delete(
+        `http://localhost:5000/api/roles/${id}`,
+        { withCredentials: true }
+      );
+      const newRoles = [...roles].filter((role) => id !== role.id);
+      setRoles(newRoles);
+      setDeleteConfirmation(false);
+    } catch (error) {
+      console.error('Error: ', error);
+    }
+  };
 
-	useEffect(() => {
-		// login();
-		getRoles();
-	}, []);
+  const deleteWarning = (role) => {
+    setRoleToDelete(role);
+    setDeleteConfirmation((now) => !now);
+    console.log(deleteConfirmation);
+  };
 
-	const handleSort = (sorters) => {
-		setSorters({ ...sorters });
-	};
+  const cancelDelete = () => {
+    setDeleteConfirmation((now) => !now);
+  };
 
-	const handleClickPage = (activePage) => {
-		setCurrentPage(activePage);
-	};
+  const handleSort = (sorters) => {
+    setSorters({ ...sorters });
+  };
 
-	const handleDetails = (role, key) => {
-		setIsModalDetails(true);
-		setCurrentRole(role);
-	};
+  const handleClickPage = (activePage) => {
+    setCurrentPage(activePage);
+  };
 
-	const handleCloseModal = () => {
-		setIsModalDetails(false);
-	};
+  const handleDetails = (role, key) => {
+    setIsModalDetails(true);
+    setCurrentRole(role);
+  };
 
-	const sortRoles = () => {
-		const rolesList = [...roles];
-		const sortedRoles = _.orderBy(rolesList, [sorters.key], [sorters.order]);
-		return sortedRoles;
-	};
+  const handleCloseModal = () => {
+    setIsModalDetails(false);
+  };
 
-	const paginateRoles = (rolesList) => {
-		const start = (currentPage - 1) * itemsPerPage;
-		// console.log(rolesList);
-		const paginatedRoles = rolesList.slice(start, start + itemsPerPage);
-		// console.log(paginatedRoles);
-		return paginatedRoles;
-	};
+  const sortRoles = () => {
+    const rolesList = [...roles];
+    const sortedRoles = _.orderBy(rolesList, [sorters.key], [sorters.order]);
+    return sortedRoles;
+  };
 
-	const sortedRoles = sortRoles();
-	const rolesToRender = paginateRoles(sortedRoles);
+  const paginateRoles = (rolesList) => {
+    const start = (currentPage - 1) * itemsPerPage;
+    const paginatedRoles = rolesList.slice(start, start + itemsPerPage);
+    return paginatedRoles;
+  };
 
-	const roleColumns = [
-		{
-			label: 'ID',
-			key: 'id',
-			sortable: 'true',
-			content: (role, key) => <th scope="row">{role[key]}</th>,
-		},
-		{
-			label: 'Title',
-			key: 'title',
-			sortable: 'true',
-			content: (role, key) => <td scope="row">{role[key]}</td>,
-		},
-		{
-			label: 'Created At',
-			key: 'created_at',
-			content: (role, key) => <td scope="row">{role[key]}</td>,
-		},
-		{
-			label: 'Action',
-			key: 'action',
-			content: (role, key) => (
-				<td scope="row">
-					<Dropdown>
-						<Dropdown.Toggle
-							variant=""
-							size="sm"
-							id="dropdown-basic"
-							bsPrefix="0"
-						>
-							{/* <i className="bi bi-pencil-square"></i> */}
-							<i className="bi bi-box-arrow-down text-success fa-lg" />
-						</Dropdown.Toggle>
+  useEffect(() => {
+    getRoles();
+  }, []);
 
-						<Dropdown.Menu>
-							<Dropdown.Item onClick={() => handleDetails(role, key)}>
-								Details
-							</Dropdown.Item>
-							<Dropdown.Item as={Link} to={`/platform/roles/update/${role.id}`}>
-								Edit
-							</Dropdown.Item>
-							{/* <Dropdown.Item onClick={() => deleteRole(role.id)}> */}
-							<Dropdown.Item onClick={() => deleteWarning(role)}>
-								Delete
-							</Dropdown.Item>
-						</Dropdown.Menu>
-					</Dropdown>
-				</td>
-			),
-		},
-	];
-	return (
-		<>
-			{isLoaded ? (
-				<div className="container text-center d-flex justify-content-center align-items-center flex-column">
-					{roles.length ? (
-						<>
-							<Modal
-								isOpen={deleteConfirmation}
-								onRequestClose={() => cancelDelete()}
-								style={{
-									overlay: {
-										position: 'fixed',
-										zIndex: '4',
-										backdropFilter: 'blur(8px)',
-									},
-									content: {
-										top: '30%',
-										left: '20%',
-										right: '20%',
-										bottom: '30%',
-										boxShadow: '3px 1px 29px -2px rgba(25,135,84,0.49)',
-										border: '1px solid #ccc',
-										overflow: 'auto',
-										borderRadius: '15px',
-										padding: '20px',
-									},
-								}}
-							>
-								<div className="container d-flex flex-column align-items-center">
-									<h1>Delete Role</h1>
-									<p>Are you sure you want to delete this role?</p>
+  const sortedRoles = sortRoles();
+  const rolesToRender = paginateRoles(sortedRoles);
 
-									<div className="clearfix">
-										<button
-											type="button"
-											className="btn btn-outline-info"
-											onClick={cancelDelete}
-										>
-											Cancel
-										</button>
-										<button
-											type="button"
-											className="btn btn-outline-danger mx-2"
-											onClick={() => deleteRole(roleToDelete.id)}
-										>
-											Delete
-										</button>
-									</div>
-								</div>
-							</Modal>
-							<div className="w-100">
-								<Table
-									items={rolesToRender}
-									columns={roleColumns}
-									sorters={sorters}
-									onSort={handleSort}
-									isModal={isModalDetails}
-									clickedRow={currentRole}
-									onCloseModal={handleCloseModal}
-									onDeleteRow={deleteRole}
-								/>
+  return (
+    <>
+      {isLoaded ? (
+        <div className="container text-center d-flex justify-content-center align-items-center flex-column">
+          {roles.length ? (
+            <>
+              <Modal
+                isOpen={deleteConfirmation}
+                onRequestClose={() => cancelDelete()}
+                style={{
+                  overlay: {
+                    position: 'fixed',
+                    zIndex: '4',
+                    backdropFilter: 'blur(8px)',
+                  },
+                  content: {
+                    top: '30%',
+                    left: '20%',
+                    right: '20%',
+                    bottom: '30%',
+                    boxShadow: '3px 1px 29px -2px rgba(25,135,84,0.49)',
+                    border: '1px solid #ccc',
+                    overflow: 'auto',
+                    borderRadius: '15px',
+                    padding: '20px',
+                  },
+                }}
+              >
+                <div className="container d-flex flex-column align-items-center">
+                  <h1>Delete Role</h1>
+                  <p>Are you sure you want to delete this role?</p>
 
-								<div className="d-flex flew-row w-25">
-									<label className="mx-2">Show:</label>
-									<select
-										className="form-select form-select-sm"
-										aria-label=".form-select-sm example"
-										onChange={(e) => setItemsPerPage(e.target.value)}
-										value={itemsPerPage}
-									>
-										{pageOptions.map((option) => (
-											<option value={option} key={option}>
-												{option}
-											</option>
-										))}
-										{/* <option defaultValue={5}>5</option>
+                  <div className="clearfix">
+                    <button
+                      type="button"
+                      className="btn btn-outline-info"
+                      onClick={cancelDelete}
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-outline-danger mx-2"
+                      onClick={() => deleteRole(roleToDelete.id)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              </Modal>
+              <div className="w-100">
+                <Table
+                  items={rolesToRender}
+                  columns={roleColumns}
+                  sorters={sorters}
+                  onSort={handleSort}
+                  isModal={isModalDetails}
+                  clickedRow={currentRole}
+                  onCloseModal={handleCloseModal}
+                  onDeleteRow={deleteRole}
+                />
+
+                <div className="d-flex flew-row w-25">
+                  <label className="mx-2">Show:</label>
+                  <select
+                    className="form-select form-select-sm"
+                    aria-label=".form-select-sm example"
+                    onChange={(e) => setItemsPerPage(e.target.value)}
+                    value={itemsPerPage}
+                  >
+                    {pageOptions.map((option) => (
+                      <option value={option} key={option}>
+                        {option}
+                      </option>
+                    ))}
+                    {/* <option defaultValue={5}>5</option>
 										<option value={10}>10</option>
 										<option value={15}>15</option> */}
-									</select>
-								</div>
-							</div>
+                  </select>
+                </div>
+              </div>
 
-							<Pagination
-								itemsPerPage={itemsPerPage}
-								totalItems={sortedRoles.length}
-								currentPage={currentPage}
-								onClickPage={handleClickPage}
-							/>
-						</>
-					) : (
-						<div
-							className="container d-flex flex-column justify-content-center align-items-center"
-							style={{ height: '50vh' }}
-						>
-							<h1 className="display-4 text-center">
-								Sorry! Roles list is empty!
-							</h1>
-							<h1 className="display-4 text-center">
-								Please create some roles first!
-							</h1>
-						</div>
-					)}
-				</div>
-			) : (
-				<div className="d-flex justify-content-center mt-5">
-					<div
-						className="spinner-border"
-						role="status"
-						style={{ height: '100px', width: '100px' }}
-					>
-						<span className="sr-only"></span>
-					</div>
-				</div>
-			)}
-		</>
-	);
+              <Pagination
+                itemsPerPage={itemsPerPage}
+                totalItems={sortedRoles.length}
+                currentPage={currentPage}
+                onClickPage={handleClickPage}
+              />
+            </>
+          ) : (
+            <div
+              className="container d-flex flex-column justify-content-center align-items-center"
+              style={{ height: '50vh' }}
+            >
+              <h1 className="display-4 text-center">
+                Sorry! Roles list is empty!
+              </h1>
+              <h1 className="display-4 text-center">
+                Please create some roles first!
+              </h1>
+            </div>
+          )}
+        </div>
+      ) : (
+        <div className="d-flex justify-content-center mt-5">
+          <div
+            className="spinner-border"
+            role="status"
+            style={{ height: '100px', width: '100px' }}
+          >
+            <span className="sr-only"></span>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Roles;
