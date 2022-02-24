fd8316dfa8aac5d759a01bb4e66bb4f6dc5350d8
Tahsin1703021 <60508726+tahsin1703021@users.noreply.github.com>
2022-02-25 02:26:26 +0600

    Merge branch 'master' of https://github.com/UitsHabib/shopon-frontend into feature/tahsin

--- src/modules/platform/user/components/users.component.js
+++ src/modules/platform/user/components/users.component.js
@@ -1,64 +1,83 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import Modal from 'react-modal';
 
-import Table from '../../../core/components/table.component';
-import Pagination from '../../../core/components/pagination.component';
-import _ from 'lodash';
-import { Link } from 'react-router-dom';
-import { useRouteMatch } from 'react-router-dom/cjs/react-router-dom.min';
-import { getUsers, deleteUser } from '../user.actions';
-import Dropdown from 'react-bootstrap/Dropdown';
-import { toast } from 'react-toastify';
+import Table from "../../../core/components/table.component";
+import Pagination from "../../../core/components/pagination.component";
+import _ from "lodash";
+import { Link } from "react-router-dom";
+import { useRouteMatch } from "react-router-dom/cjs/react-router-dom.min";
+import { getUsers, deleteUser, getUser } from "../user.actions";
+import { getPermission } from '../../permission/permission.actions';
+import Dropdown from "react-bootstrap/Dropdown";
+import { toast } from "react-toastify";
 
 const Users = (props) => {
     const { path } = useRouteMatch();
     const dispatch = useDispatch();
 
     const [sortColumn, setSortColumn] = useState({
-        path: 'profile_id',
-        order: 'asc',
+        path: "profile_id",
+        order: "asc",
     });
     const [activePage, setActivePage] = useState(1);
     const [pageCount, setPageCount] = useState(1);
     const [needToFetchUser, setNeedToFetchUser] = useState(true);
-    
+    const [modalIsOpen, setIsOpen] = useState(false);
+    const [userDetails, setUserDetails] = useState({});
+    const [ userPermissions, setUserPermissions ] = useState([]);
+    const [ showPermission, setShowPermission ] = useState(false);
+
+    const modalStyle = {
+        content: {
+          top: '30%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+        },
+      };
 
     const columns = [
         {
-            label: 'Profile ID',
-            path: 'profile_id',
+            label: "Profile ID",
+            path: "profile_id",
             sorting: true,
             content: (profile, detail) => <td>{profile[detail]}</td>,
         },
         {
-            label: 'First Name',
-            path: 'first_name',
+            label: "First Name",
+            path: "first_name",
             sorting: true,
             content: (profile, detail) => <td>{profile[detail]}</td>,
         },
         {
-            label: 'Last Name',
-            path: 'last_name',
+            label: "Last Name",
+            path: "last_name",
             sorting: true,
             content: (profile, detail) => <td>{profile[detail]}</td>,
         },
         {
-            label: 'Email',
-            path: 'email',
+            label: "Email",
+            path: "email",
             sorting: true,
             content: (profile, detail) => <td>{profile[detail]}</td>,
         },
         {
-            label: 'Phone No.',
-            path: 'phone',
+            label: "Phone No.",
+            path: "phone",
             content: (profile, detail) => <td>{profile[detail]}</td>,
         },
         {
-            label: 'Actions',
+            label: "Actions",
             content: (profile, detail) => (
                 <td>
                     <Dropdown>
-                        <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                        <Dropdown.Toggle
+                            variant="secondary"
+                            id="dropdown-basic"
+                        >
                             <i className="bi bi-pencil-square"></i>
                         </Dropdown.Toggle>
 
@@ -66,16 +85,31 @@ const Users = (props) => {
                             <Dropdown.Item>
                                 <Link
                                     to={{
-                                        pathname: path + '/' + profile.id + '/update',
-                                        state: { prevPath: props.location.pathname, data: profile.id },
+                                        pathname:
+                                            path + "/" + profile.id + "/update",
+                                        state: {
+                                            prevPath: props.location.pathname,
+                                            data: profile.id,
+                                        },
                                     }}
-                                    style={{ textDecoration: 'none', color: 'black' }}
+                                    style={{
+                                        textDecoration: "none",
+                                        color: "black",
+                                    }}
                                 >
                                     UPDATE
                                 </Link>
                             </Dropdown.Item>
-                            <Dropdown.Item disabled={disableDeleteUser(profile.id)} onClick={() => handleDeleteUser(profile.id)}>Delete</Dropdown.Item>
-                            <Dropdown.Item onClick={() => handleShowDetails(profile.id)}>Details</Dropdown.Item>
+                            <Dropdown.Item
+                                onClick={() => handleDeleteUser(profile.id)}
+                            >
+                                Delete
+                            </Dropdown.Item>
+                            <Dropdown.Item
+                                onClick={() => handleShowDetails(profile.id)}
+                            >
+                                Details
+                            </Dropdown.Item>
                         </Dropdown.Menu>
                     </Dropdown>
                 </td>
@@ -83,21 +117,36 @@ const Users = (props) => {
         },
     ];
 
-    const users = useSelector(state => state.userReducer.users);
-    const loggedInUser = useSelector(state => state.userReducer.loggedInUser.id);
-    
+    const users = useSelector((state) => state.userReducer.users);
+    const loggedInUser = useSelector(
+        (state) => state.userReducer.loggedInUser.id
+    );
+
     const handleSort = (sortColumn) => setSortColumn(sortColumn);
 
-    const disableDeleteUser = (id) => {
-        console.log(id);
-        console.log(loggedInUser);
-        
-    }
     const handleShowDetails = (id) => {
-        
-    }
+        setIsOpen(true);
+        try{
+            getUser(id)
+            .then(res => setUserDetails(res.data))
+        }catch (err){console.log('err getting user');}
+    };
+
+    const handleUserPermission = (id) => {
+        try{
+            getPermission(id)
+            .then(res => {
+                console.log(res.data.permission_services);
+                setUserPermissions(res.data.permission_services);
+            })
+        }catch (err){console.log('err getting user permission');}
+    };
     const sortUsers = (users) => {
-        const sortedUsers = _.orderBy(users, [sortColumn.path], [sortColumn.order]);
+        const sortedUsers = _.orderBy(
+            users,
+            [sortColumn.path],
+            [sortColumn.order]
+        );
         return sortedUsers;
     };
 
@@ -125,18 +174,76 @@ const Users = (props) => {
 
     const paginatedUsers = paginateUsers();
     const userList = sortUsers(paginatedUsers);
-
+    const handleCloseModal = () => {
+        
+    }
     return (
         <div className="container">
-            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Modal
+                isOpen={modalIsOpen}
+                style={modalStyle}
+                contentLabel="Details Modal"
+            >
+                <button onClick={() => setIsOpen(false)}>close</button>
+                <div>
+                    <ul>
+                        {
+                            <>
+                                <li>ID: {userDetails.id}</li>
+                                <li>First Name: {userDetails.first_name}</li>
+                                <li>Last Name: {userDetails.last_name}</li>
+                                <li>Email: {userDetails.email}</li>
+                                <li>Phone No: {userDetails.phone}</li>
+                                <li>Profile Slug: {userDetails.profile_id}</li>
+                            </>
+                        }
+                    </ul>
+                    <div>
+                        <button onClick={()=> {
+                            handleUserPermission(userDetails.profile.profile_permissions[0].permission_id);
+                            setShowPermission(!showPermission)
+                        }
+                        }>{showPermission === false ? 'Show Permissions' : 'Close'}</button>
+                        {
+                            showPermission === true ? (
+                                <ul>
+                                    {
+                                        userPermissions.map(permission=> {
+                                            return <li key={permission.service.id}>{permission.service.title}</li>;
+                                        }
+                                            
+                                        )
+                                    }
+                                </ul>
+                            ): null
+                        }    
+                    </div>
+                   
+                </div>
+            </Modal>
+
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
                 <div></div>
-                <Link to="/platform/users/create" className="btn btn-primary m-2">
-                    Create User{' '}
+                <Link
+                    to="/platform/users/create"
+                    className="btn btn-primary m-2"
+                >
+                    Create User{" "}
                 </Link>
             </div>
 
-            <Table users={userList} columns={columns} sortColumns={sortColumn} onSort={handleSort} />
-            <Pagination totalUsers={users.length} pageCount={pageCount} activePage={activePage} onClickPage={handleClickPage} />
+            <Table
+                users={userList}
+                columns={columns}
+                sortColumns={sortColumn}
+                onSort={handleSort}
+            />
+            <Pagination
+                totalUsers={users.length}
+                pageCount={pageCount}
+                activePage={activePage}
+                onClickPage={handleClickPage}
+            />
         </div>
     );
 };
