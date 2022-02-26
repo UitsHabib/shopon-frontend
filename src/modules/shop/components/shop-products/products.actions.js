94c0ac1733bfa1f374fdc0a3575101bf4835cc50
sbswarna <shatabdibiswasswarna@gmail.com>
2022-02-26 21:35:20 +0600

    Fix sorting.

--- src/modules/shop/components/shop-products/products.actions.js
+++ src/modules/shop/components/shop-products/products.actions.js
@@ -3,12 +3,17 @@ import Types from "./shop-products.types";
 
 const baseUrl = "http://localhost:5000";
 
-export function getAllProducts() {
+export function getAllProducts(page, limit, orderBy, orderType) {
+    const url = `${baseUrl}/api/shops/products?page=${page}`
+        + (limit ? `&limit=${limit}` : '')
+        + (orderBy ? `&orderBy=${orderBy}` : '')
+        + (orderType ? `&orderType=${orderType}` : '');
+        
     return {
         type: Types.GET_SHOP_PRODUCT,
         payload: axios({
             method: "get",
-            url: `${baseUrl}/api/shops/products`,
+            url,
             withCredentials: "true",
         }),
     };
